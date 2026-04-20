865a6ad6bba37f9db976a9a3bd5f3ada1268c6ef
VishalDevkate <v.devkate@gmail.com>
2026-04-20 11:21:55 +0530

    pyTest Demos

--- Functions.py
+++
@@ -1,4 +0,0 @@
-def add_integers(num1, num2):
-    return num1 + num2
-print(add_integers(1, 2))
-
--- IfElse.py
+++
@@ -1,8 +0,0 @@
-a = 6
-
-if a > 7:
-    print("a is greater than 7")
-else:
-    print("a is not greater than 7")
-
-print("execution completed")
